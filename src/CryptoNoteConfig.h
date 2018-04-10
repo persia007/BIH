f163975e02e931b78c7862291267d7009be2563a
Persia <38175935+persia007@users.noreply.github.com>
2018-04-10 14:29:05 +0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -2,3 +2,7 @@ const char CRYPTONOTE_NAME[] = "Bihcoin";
 const uint64_t MONEY_SUPPLY = (uint64_t)(-1);
 const unsigned EMISSION_SPEED_FACTOR = 18;
 const uint64_t DIFFICULTY_TARGET = 120;
+const std::initializer_list<const char*> SEED_NODES = {
+  "111.11.11.11:17236",
+  "222.22.22.22:17236",
+};
