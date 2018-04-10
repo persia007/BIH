eeae249b725dc41691e6c293ed6da805e30f0eaa
Persia <38175935+persia007@users.noreply.github.com>
2018-04-10 14:35:11 +0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -7,3 +7,8 @@ const std::initializer_list<const char*> SEED_NODES = {
   "222.22.22.22:17236",
 };
 const uint64_t MINIMUM_FEE = 100000;
+const size_t CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE = 20000;
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0xe9; // addresses start with "0x494f"
+const char GENESIS_COINBASE_TX_HEX[] = "";
+Bihcoind --print-genesis-tx
+const char GENESIS_COINBASE_TX_HEX[] = "013c01ff0001ffff...785a33d9ebdba68b0";
