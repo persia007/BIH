7dfcb70ba1f9c68adc272db62c508a43fd7e9099
Persia <38175935+persia007@users.noreply.github.com>
2018-04-10 14:23:46 +0400

    Update Currency.cpp

--- src/src/CryptoNoteCore/Currency.cpp
+++ src/src/CryptoNoteCore/Currency.cpp
@@ -1,1 +1,3 @@
 bool Currency::getBlockReward(size_t medianSize, size_t currentBlockSize, uint64_t alreadyGeneratedCoins, uint64_t fee, uint64_t& reward, int64_t& emissionChange) const
+const int P2P_DEFAULT_PORT = 17236;
+const int RPC_DEFAULT_PORT = 18236;
